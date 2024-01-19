554a404b72da8e59071c96fb24244fd864e7ddcc
fbragee <145098995+fbragee@users.noreply.github.com>
2024-01-19 08:38:58 +0000

    Last touches

--- app.js
+++ app.js
@@ -3,34 +3,68 @@ function getComputerChoice() {
 	if (random > 3 && random < 6) {
 		return 'PAPER';
 	} else if (random > 6 && random < 9) {
-		return 'SCISSORS';
+		return 'ScisSors';
 	} else {
-		return 'ROCK';
+		return 'rock';
 	}
 }
 
 function playRound(playerSelection, computerSelection) {
-	if (playerSelection == 'rock' && computerSelection == 'rock') {
+	let player = playerSelection.toLowerCase();
+	let computer = computerSelection.toLowerCase();
+	if (player === computer) {
 		return 'TIE, re-play round';
-	} else if (playerSelection == 'paper' && computerSelection == 'paper') {
-		return 'TIE, re-play round';
-	} else if (
-		playerSelection == 'scissors' &&
-		computerSelection == 'scissors'
-	) {
-		return 'TIE, re-play round';
-	} else if (playerSelection == 'rock' && computerSelection == 'paper') {
+	} else if (player == 'rock' && computer == 'paper') {
 		return 'You lose! Paper beats Rock';
-	} else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+	} else if (player == 'paper' && computer == 'scissors') {
 		return 'You lose! Scissors beats Paper';
-	} else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+	} else if (player == 'scissors' && computer == 'rock') {
 		return 'You lose! Rock beats Scissors!';
-	} else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+	} else if (player == 'rock' && computer == 'scissors') {
 		return 'Computer loses! Rock beats Scissors';
-	} else if (playerSelection == 'paper' && computerSelection == 'rock') {
-		return 'Computer Loses';
+	} else if (player == 'paper' && computer == 'rock') {
+		return 'Computer loses! Paper beats Rock';
+	} else if (player == 'scissors' && computer == 'paper') {
+		return 'Computer loses! Scissors beats Paper';
 	}
 }
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
+function game() {
+	let score = 0;
+	let win = 0,
+		tie = 0,
+		lose = 0;
+
+	for (let i = 0; i < 5; i++) {
+		const playerSelection = prompt(`Round${i + 1} Choice?`);
+		const computerSelection = getComputerChoice();
+		if (
+			playRound(playerSelection, computerSelection) ==
+				'Computer Loses! Paper beats Rock' ||
+			playRound(playerSelection, computerSelection) ==
+				'Computer Loses! Scissors beats Paper' ||
+			playRound(playerSelection, computerSelection) ==
+				'Computer loses! Rock beats Scissors'
+		) {
+			playerScore++;
+		} else if (
+			playRound(playerSelection, computerSelection) ==
+				'You lose! Paper beats Rock' ||
+			playRound(playerSelection, computerSelection) ==
+				'You lose! Scissors beats Paper' ||
+			playRound(playerSelection, computerSelection) ==
+				'You lose! Rock beats Scissors'
+		) {
+			computerScore++;
+		} else {
+			playerScore = playerScore + 0;
+			computerScore = computerScore + 0;
+		}
+	}
+
+	if (playerScore === 5) {
+		console.log('You won!!!');
+	} else if (computerScore === 5) {
+		console.log('The computer wins...');
+	}
+}
